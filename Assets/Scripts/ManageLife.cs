6c0081a85c343f584942c00fa4bfb60b5c9293cc
Romain <82060667+kiprokoala@users.noreply.github.com>
2023-04-25 19:32:54 +0200

    système de lifebar

---
+++ Assets/Scripts/ManageLife.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class ManageLife : MonoBehaviour
+{
+    private int health = 10;
+    public Lifebar lifebar;
+    private void Start()
+    {
+        lifebar.SetMaxHealth(health);   
+    }
+    private void Update()
+    {
+        //Mort
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            health--;
+            lifebar.setHealth(health);
+        }
+        //Pouvoir
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            health++;
+            lifebar.setHealth(health);
+        }
+    }
+}
